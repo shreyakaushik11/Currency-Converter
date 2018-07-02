30ec8005803f00ef4ccc7464bd399e145edbb8f1
Shreya Kaushik <shreyakaushik11@gmail.com>
2018-07-02 10:21:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  url="https://exchangeratesapi.io/api/latest?base=";
+  base=[];
+  i;
+  number;
+  answer;
+  x;
+  base_code;
+  n;
+  base_value;
+  target_value;
+  constructor(private http: HttpClient){
+
+  }
+  ngOnInit(){
+    this.getData();
+  }
+  getData(){
+    this.http.get(this.url).subscribe((data)=>{
+      this.base.push(data['base']);
+      // console.log(data.keys(rates);
+      for (let prop in data['rates']) {
+        this.base.push(prop);
+        console.log(this.base);
+       
+      }
+    })
+  }
+  getAnswer(){
+      this.http.get(this.url+this.base_value).subscribe((data)=>{
+      this.answer=data['rates'][this.target_value]*this.number;
+      console.log(this.answer);
+  })
+  }
 }
